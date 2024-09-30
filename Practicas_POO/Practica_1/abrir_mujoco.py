36f8c9039d7b18511ce7af284fb9af2f46d9eb78
gabi-er <ger1001@alu.ubu.es>
2024-09-30 20:33:27 +0200

    Abre ventana con OpenGL

--- Practicas_POO/Practica_1/abrir_mujoco.py
+++ Practicas_POO/Practica_1/abrir_mujoco.py
@@ -1,17 +1,41 @@
-import mujoco
+import mujoco as mj
+from mujoco.glfw import glfw
+import numpy as np
 
-xml = """
-<mujoco>
-  <worldbody>
-    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
-    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
-  </worldbody>
-</mujoco>
-"""
-# Make model and data
-model = mujoco.MjModel.from_xml_string(xml)
-data = mujoco.MjData(model)
 
-# Make renderer, render and show the pixels
-with mujoco.Renderer(model) as renderer:
-  ope.show_image(renderer.render())
+
+def open_window(): # Iniciar OpenGL
+    try:
+        glfw.init()
+    except:
+        raise("Error al iniciar glfw")
+    
+    # Crear y manejar error ventana
+    global window
+    window = glfw.create_window(960,540,"MuJoCo: Motor de Fisicas", None, None) 
+    
+    if window == False:  
+        glfw.terminate()
+        raise("Error al abrir la ventana de la aplicación")
+    
+    # Establecer el contexto de OpenGL
+    glfw.make_context_current(window)
+    glfw.swap_interval(1)  # V-Sync (1) = On
+def mouse():
+    glfw.set_cursor_pos_callback(window)
+   
+def main():
+    open_window()
+    while glfw.window_should_close(window) == False:
+        #Render here
+        mouse()
+        glfw.swap_buffers(window)
+        glfw.poll_events()
+
+    glfw.terminate()
+    
+
+
+
+if __name__ == "__main__":
+    main()
